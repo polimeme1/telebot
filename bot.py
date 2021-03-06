04f35212b6e65a63e5ee5fe0fc5b249f43f7257e
polimeme <chechurovapolina@mail.ru>
2021-03-06 22:34:28 +0300

    with a sticker

--- bot.py
+++ bot.py
@@ -4,15 +4,16 @@ from database import *
 
 bot = telebot.TeleBot(config.TOKEN)
 
-
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands = ["start"])
 def welcome(message):
+    sticker = open(r"./stickers/hello.webp", "rb")
+    bot.send_sticker(message.chat.id, sticker)
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
     keyboard.row("1", "2", "3", "4")
     bot.send_message(message.chat.id, "Привет, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот-расписание для потока Бизнес-информатики."
                      .format(message.from_user, bot.get_me()), parse_mode = "html", reply_markup=keyboard)
 
-@bot.message_handler(commands=["help"])
+@bot.message_handler(commands = ["help"])
 def helpme(message):
     bot.send_message(message.chat.id, "Я - бот, который показывает расписание для всего потока на каждый день.\n"
                                       "Для того, чтобы посмотреть расписание, нажми на последнюю цифру номера группы."
